6cadc4a28d12857dd2cf379debe136a6c8d7078f
brokiem <milyhqvideo@gmail.com>
2022-12-22 18:24:31 +0800

    some fixes

--- chat-app/src/components/Chats.vue
+++ chat-app/src/components/Chats.vue
@@ -1,12 +1,12 @@
 <template>
   <main>
     <div class="bg-gray-700/60 h-[100%]">
-      <div class="h-screen flex  flex-col overflow-y-auto">
+      <div class="flex flex-col overflow-y-auto">
         <nav class="bg-gray-700 sticky p-3 top-0 w-full z-20">
           <span class="text-lg font-medium text-gray-700 dark:text-white">{{activeChannel?.title ?? "undefined"}}</span>
         </nav>
 
-        <div class="flex flex-col ml-6 mt-5">
+        <div class="h-[80vh] flex flex-col ml-6 overflow-y-scroll">
           <Chat v-if="isLoaded" v-for="{text, photoURL, displayName} in messages"
                 :author="displayName"
                 :photo-u-r-l="photoURL"
@@ -16,11 +16,9 @@
           <div ref="dummy"></div>
         </div>
 
-        <div class="bottom-0 pb-6 ml-4 mr-4 sticky bg-[#2B313A] rounded-t-lg z-20">
-          <form @submit.prevent="onSubmit">
-            <input type="text" id="input-message" class="block p-3 text-sm text-gray-900 w-full outline-none rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Message" required>
-          </form>
-        </div>
+        <form @submit.prevent="onSubmit" class="bottom-0 mb-6 ml-4 mr-4 sticky bg-[#2B313A] rounded-lg z-20">
+          <input type="text" id="input-message" class="block p-3 text-sm text-gray-900 w-full outline-none rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Message" required>
+        </form>
       </div>
     </div>
   </main>
@@ -67,13 +65,18 @@ export default {
       })
     }
   },
+  watch: {
+    messages() {
+      this.$nextTick(() => {
+        this.$refs?.dummy?.scrollIntoView({ behavior: "smooth" })
+      })
+    }
+  },
   methods: {
     onSubmit() {
       const input = document.getElementById("input-message")
 
       this.sendMessage(this.activeChannel.id, this.user.uid, this.user.displayName, this.user.photoURL, input.value)
-
-      this.$refs.dummy.scrollIntoView({ behavior: "smooth" })
 
       input.value = ""
     }
