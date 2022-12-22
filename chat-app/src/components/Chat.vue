4f7ac02b882fbf05757a1b43811ba1e80a5253df
brokiem <milyhqvideo@gmail.com>
2022-12-23 03:17:17 +0800

    Simplify

--- chat-app/src/components/Chat.vue
+++ chat-app/src/components/Chat.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="my-1">
     <div v-if="!headless" class="flex ml-5">
-      <div class="overflow-hidden relative w-11 h-11 bg-gray-100 rounded-full dark:bg-gray-600">
+      <div class="overflow-hidden relative w-11 h-11 rounded-full">
         <img class="absolute h-full w-full object-cover" :src="photoURL" alt="avatar">
       </div>
 
@@ -19,14 +19,12 @@
       </div>
     </div>
 
-    <div v-if="headless" class="ml-16 flex flex-nowrap flex-col">
-      <div class="ml-4">
-        <div class="flex flex-col space-y-2 max-w-[65vw] items-start">
-          <div>
-              <span class="px-3 py-1.5 rounded-md inline-block bg-gray-300 text-gray-700">
-                {{message}}
-              </span>
-          </div>
+    <div v-if="headless" class="ml-20 flex flex-nowrap flex-col">
+      <div class="flex flex-col max-w-[65vw] items-start">
+        <div>
+          <span class="px-3 py-1.5 rounded-md inline-block bg-gray-300 text-gray-700">
+            {{message}}
+          </span>
         </div>
       </div>
     </div>
